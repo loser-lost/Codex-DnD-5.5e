957b26996697a3869f1545ac87752ae0feb32d09
Michael Israel Sousa Santos <43841525+loser-lost@users.noreply.github.com>
2025-07-30 15:17:37 -0300

    ✨  Feat: Paladin filter

--- components/comp/drawerFilter.tsx
+++ components/comp/drawerFilter.tsx
@@ -2,7 +2,7 @@ import { ReactElement, memo } from 'react';
 import { Drawer, DrawerGroup, DrawerItem, CheckBox, IconProps } from '@ui-kitten/components';
 
 const SCHOOLS = ['Abjuração', 'Evocação', 'Ilusão', 'Transmutação', 'Necromancia', 'Encantamento', 'Adivinhação'];
-const CLASSES = ['Mago', 'Feiticeiro', 'Clérigo', 'Guardião', 'Bardo', 'Druida', 'Bruxo',];
+const CLASSES = ['Mago', 'Feiticeiro', 'Clérigo', 'Guardião', 'Bardo', 'Druida', 'Bruxo','Paladino'];
 const RANGES = ['Pessoal', 'Toque', '3 metros', '4,5 metros', '9 metros', '18 metros', '27 metros', '36 metros', '45 metros', '90 metros', "1,5 km", "800 quilômetros", "Ilimitado", "Especial"];
 const TIMES = ['Ação', 'Ação ou Ritual', 'Ação Bônus', '1 minuto ou Ritual', '10 minutos', '1 minuto', '1 hora', '8 horas', '24 horas', 'Ação Bônus, que você realiza imediatamente após acertar um alvo com uma arma Corpo a Corpo ou um Ataque Desarmado', 'Ação Bônus, que você realiza imediatamente após atingir uma criatura com uma arma'];
 const CIRCLES = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
