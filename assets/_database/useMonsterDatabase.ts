ff88488a0d07d5530b11e92c5b6b32dfc44b313a
Michael Israel Sousa Santos <43841525+loser-lost@users.noreply.github.com>
2026-03-05 01:53:13 -0300

    ✨feat: read function  monster crated

--- assets/_database/useMonsterDatabase.ts
+++ assets/_database/useMonsterDatabase.ts
@@ -1,42 +1,47 @@
 import { useSQLiteContext } from 'expo-sqlite';
 
-export type spellDatabase ={
- //added livro and linguagem
+export type monster5eINDatabase ={
+    name: string,
+    ArmorClass: string,
+    HitPoints: string,
+    STR: string,
+    STR_mod: string,
+    DEX: string,
+    DEX_mod: string,
+    CON: string,
+    CON_mod: string,
+    INT: string,
+    INT_mod: string,
+    WIS: string,
+    WIS_mod: string,
+    CHA: string,
+    CHA_mod: string,
+    SavingThrows: string,
+    Skills: string,
+    Senses: string,
+    Languages: string,
+    Challenge: string,
+    Traits: string,
+    Actions: string,
+    LegendaryActions: string,
+    img_url: string,
   
 }
 
-export function useSpellDatabase() {
+export function monster5eINData() {
     const db = useSQLiteContext();
-
-
-    async function create(date: Omit<spellDatabase, "id">){
-
-    }
 
     async function read() {
     try {
-       
+        const query = 'SELECT * FROM monster5eIN';
+        const response = await db.getAllAsync<monster5eINDatabase>(query);
+        return response;
     } catch (error) {
-        console.error('', error);
-        throw error;
+        console.error('Erro ao buscar monstros:', error);
+        return [];
     }
-    }
-    
-    async function update() {
-        
-    }
-
-    async function remove() {
-        try {
-            
-        } catch (error) {
-            console.error('', error); 
-        }  
     }
     return{
-        create,
-        read,
-        update,
-        remove
+        read
     }
 }
